10884d3399be958376d96eaf2d54f0fab4936ab1
AndriesDeGraeff <andriesdg199@gmail.com>
2020-11-13 23:41:28 +0100

    Mana and Plank

--- Assets/Scripts/CreateObject.cs
+++ Assets/Scripts/CreateObject.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using UnityEngine.UI;
+using System.Collections;
 using UnityEngine;
 
 public class CreateObject : MonoBehaviour
@@ -6,110 +7,126 @@ public class CreateObject : MonoBehaviour
     public Transform Spawnpoint;
     public GameObject PrefabCyl;
     public GameObject PrefabCub;
+    public GameObject PrefabPlnk;
     public GameObject TempParent;
     public GameObject Casting;
+    public Slider slider;
     float ObjCounter;
 
     public Camera Cam;
-    public float range = 100f;
-    bool grabbed;
+    private float currentValue = 0f;
 
+    private void Start()
+    {
+        CurrentValue = 1000f;
+    }
 
-    // Start is called before the first frame update
     void Update() {
-        if (Input.GetKeyDown("1") && ObjCounter < 3)
-        {
+
+        if (Input.GetKeyDown("1") && ObjCounter < 3 && currentValue > 100) {
             Instantiate(PrefabCub, Spawnpoint.position, Spawnpoint.rotation);
 
             ObjCounter += 1;
+            CurrentValue -= 100f;
         }
-        else
-        {
-            Casting.SetActive(true); 
+        else if (Input.GetKeyDown("1") && (ObjCounter == 3 || currentValue < 100)) {
+            StartCoroutine("CantCast");
         }
 
-        if (Input.GetKeyDown("2") && ObjCounter < 3) {
+        if (Input.GetKeyDown("2") && ObjCounter < 3 && currentValue > 100) {
             Instantiate(PrefabCyl, Spawnpoint.position, Spawnpoint.rotation);
 
             ObjCounter += 1;
+            CurrentValue -= 100f;
+        }
+        else if (Input.GetKeyDown("2") && (ObjCounter == 3 || currentValue < 100)) {
+            StartCoroutine(CantCast());
         }
 
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetKeyDown("3") && ObjCounter < 3 && currentValue > 100)
         {
+            Instantiate(PrefabPlnk, Spawnpoint.position, Spawnpoint.rotation);
+
+            ObjCounter += 1;
+            CurrentValue -= 100f;
+        }
+        else if (Input.GetKeyDown("3") && (ObjCounter == 3 || currentValue < 100))
+        {
+            StartCoroutine(CantCast());
+        }
+
+        if (Input.GetButtonDown("Fire1")) {
             GrowShoot();
         }
 
-        if (Input.GetButtonDown("Fire2"))
-        {
+        if (Input.GetButtonDown("Fire2")) {
             ShrinkShoot();
         }
 
-        if (Input.GetKeyDown("q"))
-        {
+        if (Input.GetKeyDown("q")) {
             RaycastHit kill;
-            if (Physics.Raycast(Cam.transform.position, Cam.transform.forward, out kill, range))
-            {
+            if (Physics.Raycast(Cam.transform.position, Cam.transform.forward, out kill)) {
                 Target target = kill.transform.GetComponent<Target>();
                 target.Kill = true;
+
+                CurrentValue += 100f + target.pressed * 10f;
+
                 ObjCounter -= 1;
             }
         }
 
-        if (Input.GetKeyDown("e"))
-        {
-
+        if (Input.GetKeyDown("e")) {
             RaycastHit hit;
-            if (Physics.Raycast(Cam.transform.position, Cam.transform.forward, out hit, range))
-            {
+            if (Physics.Raycast(Cam.transform.position, Cam.transform.forward, out hit)) {
                 hit.transform.GetComponent<Rigidbody>().useGravity = false;
                 hit.transform.GetComponent<Rigidbody>().isKinematic = true;
                 hit.transform.position = Spawnpoint.transform.position;
                 hit.transform.rotation = Spawnpoint.transform.rotation;
                 hit.transform.parent = TempParent.transform;
-
-                grabbed = true;
             }
         }
 
-        if (Input.GetKeyUp("e"))
-        {
+        if (Input.GetKeyUp("e")) {
             RaycastHit hit;
-            if (Physics.Raycast(Cam.transform.position, Cam.transform.forward, out hit, range))
-            {
+            if (Physics.Raycast(Cam.transform.position, Cam.transform.forward, out hit)) {
                 hit.transform.GetComponent<Rigidbody>().useGravity = true;
                 hit.transform.GetComponent<Rigidbody>().isKinematic = false;
                 hit.transform.position = Spawnpoint.transform.position;
                 hit.transform.parent = null;
-
-                grabbed = false;
             }
         }
     }
 
-    void GrowShoot()
-    {
+    void GrowShoot() {
         RaycastHit hit;
-        if (Physics.Raycast(Cam.transform.position, Cam.transform.forward, out hit, range))
-        {
-            Debug.Log(hit.transform.name);
-
+        if (Physics.Raycast(Cam.transform.position, Cam.transform.forward, out hit)) {
             Target target = hit.transform.GetComponent<Target>();
-            if (target != null) target.Grow();
+            if (target != null && CurrentValue > 10) CurrentValue -= 10;
+            if (target != null && CurrentValue > 10) target.Grow();
         }
-
     }
-    void ShrinkShoot()
-    {
+    void ShrinkShoot() {
         RaycastHit hit;
-        if (Physics.Raycast(Cam.transform.position, Cam.transform.forward, out hit, range))
-        {
-            Debug.Log(hit.transform.name);
-
+        if (Physics.Raycast(Cam.transform.position, Cam.transform.forward, out hit)) {
             Target target = hit.transform.GetComponent<Target>();
+            if (target != null) CurrentValue += 10;
             if (target != null) target.Shrink();
         }
-
     }
 
+    IEnumerator CantCast() {
+        Casting.SetActive(true);
+        yield return new WaitForSeconds(3);
+        Casting.SetActive(false);
+    }
 
+    public float CurrentValue {
+        get {
+            return currentValue;
+        }
+        set {
+            currentValue = value;
+            slider.value = currentValue;
+        }
+    }
 }
--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
     Vector3 scaleChange = new Vector3(0.01f, 0.01f, 0.01f);
+    Vector3 plankChange = new Vector3(.07f, .003f, .02f);
     bool Grown;
+    public bool Plank;
     public bool Kill;
+    public float pressed;
 
     private void Start()
     {
@@ -18,11 +21,23 @@ public class Target : MonoBehaviour
 
     private void Update()
     {
-        if (transform.localScale.y < 1f && !Grown)
+        if (transform.localScale.y < 1f && !Grown && !Plank)
         {
             transform.localScale += scaleChange;
         }
-        else
+        else if (!Plank)
+        {
+            Grown = true;
+
+            GetComponent<Rigidbody>().useGravity = true;
+            GetComponent<Rigidbody>().isKinematic = false;
+        }
+
+        if (transform.localScale.y < .3f && !Grown && Plank)
+        {
+            transform.localScale += plankChange;
+        }
+        else if(Plank)
         {
             Grown = true;
 
@@ -39,15 +54,24 @@ public class Target : MonoBehaviour
         {
             Destroy(gameObject);
         }
+
+        if (transform.localScale.y < 0.2f && Grown)
+        {
+            Destroy(gameObject);
+        }
     }
 
     public void Grow()
     {
         transform.localScale *= 1.1f;
+
+        pressed += 1;
     }
 
     public void Shrink()
     {
         transform.localScale *= .9f;
+
+        pressed -= 1;
     } 
 }
